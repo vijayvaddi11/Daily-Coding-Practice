8bf5be5ac12419853071fc3cc257254da80eef08
vijay vaddi <vijayjay6688@gmail.com>
2025-05-02 18:38:04 +0530

    Add Day-12 of daily-coding-practice

--- Star pattern 1.py
+++ Star pattern 1.py
@@ -1,4 +1,10 @@
 # Star Pattern -1
+'''
+   ****
+   ****
+   ****
+   ****
+'''
 
 for i in range(4):
     for j in range(4):
---
+++ Star pattern 2.py
@@ -0,0 +1,15 @@
+# Star Pattern -2
+'''
+   ****
+   *  *
+   *  *
+   ****
+'''
+
+for i in range(4):
+    for j in range(4):
+        if i==0 or i==3 or j==0 or j==3:
+            print('*', end="")
+        else:
+            print(' ',end="")
+    print()        
