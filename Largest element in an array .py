ad35d537e05a1811e037e3d669e73894bb236800
Vijay Vaddi <141708089+vijayvaddi11@users.noreply.github.com>
2025-06-23 10:09:56 +0530

    Update Largest element in an array .py

--- Largest element in an array .py
+++ Largest element in an array .py
@@ -1,5 +1,8 @@
 # Q26.Find Largest element in an array
+input_arr = list(map(int,input().split()))
+print(max(input_arr))
 
+'''
 input_arr = list(map(int,input().split()))
 max_element = input_arr[0]
 for num in input_arr:
@@ -7,9 +10,4 @@ for num in input_arr:
         max_element = num
 print(max_element)        
 
-#print(max(input_arr))
-
-#or
-a=sorted(input_arr)
-len_arr = len(input_arr)
-print(a[len_arr-1])
+'''
