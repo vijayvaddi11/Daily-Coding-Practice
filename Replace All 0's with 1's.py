0550873f187b681aab2238b1d3448c2d0f84f8f6
Vijay Vaddi <141708089+vijayvaddi11@users.noreply.github.com>
2025-06-23 08:49:03 +0530

    Update Replace All 0's with 1's.py

--- Replace All 0's with 1's.py
+++ Replace All 0's with 1's.py
@@ -1,5 +1,24 @@
 # Q19. Replace All 0’s With 1 In A Given Integer
 
+
+number = int(input())
+str_num = str(number)
+print(str_num.replace('0','1'))
+
+
+'''
+number = int(input())
+
+str_num = str(number)
+
+for i in range(0,len(str_num)):
+    if str_num[i]=="0":
+        str_num=str_num.replace('0','1')
+print(str_num)        
+
+
+
+
 number = input()
 li_num = list(number)
 output =""
@@ -9,4 +28,4 @@ for num in li_num:
     else:
         str_num = str(num)
         output += str_num
-print(int(output))
+print(int(output))'''
