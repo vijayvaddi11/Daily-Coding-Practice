85534b1b543840a9b42ca23f94f971bb17fb0a3a
vijay vaddi <vijayjay6688@gmail.com>
2025-06-26 10:55:07 +0530

    add pattern.py

---
+++ star pattern 5.py
@@ -0,0 +1,13 @@
+'''
+   *-*-*  
+   *-*-*  
+   *-*-*
+'''
+
+for i in range(3):
+    for j in range(3):
+        if i in[0,1,2] and j==2:
+            print('*', end =" ")
+        else:    
+            print('*', end ="-")
+    print(" ")    
