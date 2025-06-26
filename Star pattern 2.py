8fe2f942044919d13d0c9ecc52b4ba775c327490
vijay vaddi <vijayjay6688@gmail.com>
2025-06-26 11:29:30 +0530

    updated pattern

--- Star pattern 2.py
+++ Star pattern 2.py
@@ -13,3 +13,19 @@ for i in range(4):
         else:
             print(' ',end="")
     print()        
+    
+    
+    
+    
+    
+    
+    
+    
+# for i in range(5):
+#     for j in range(5):
+#         if i in [1,2,3] and j in [1,2,3]:
+#         #if 1 <= i <= 3 and 1 <= j <= 3:    
+#                 print(' ',end='')
+#         else:        
+#             print('*', end='')
+#     print('')        
