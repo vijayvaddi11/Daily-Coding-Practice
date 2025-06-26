3d9e70c38990bd7e97d3701096343e576a471880
vijay vaddi <vijayjay6688@gmail.com>
2025-06-26 11:04:00 +0530

    updated start pattern

--- star pattern 5.py
+++ star pattern 5.py
@@ -6,7 +6,7 @@
 
 for i in range(3):
     for j in range(3):
-        if i in[0,1,2] and j==2:
+        if j==2:
             print('*', end =" ")
         else:    
             print('*', end ="-")
