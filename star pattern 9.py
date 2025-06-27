ffc4cbf2935de20e8d9cd8d7e44f204736d15f2e
vijay vaddi <vijayjay6688@gmail.com>
2025-06-27 12:21:33 +0530

    add star pattern 9

---
+++ star pattern 9.py
@@ -0,0 +1,22 @@
+'''
+    *
+   ***
+  *****
+ *******
+*********
+
+'''
+
+
+
+
+r=5
+n=r-1
+for i in range(r):
+    for j in range(n-i):
+        print(' ',end='')
+    temp = i*2+1
+    for i in range(temp):
+        print('*',end='')
+
+    print('')    
