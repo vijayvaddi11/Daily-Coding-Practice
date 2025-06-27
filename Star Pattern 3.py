43e5eb112cb4d451089b5eed2a9e3a344c18d84d
vijay vaddi <vijayjay6688@gmail.com>
2025-06-27 10:56:16 +0530

    updated pattern 3

--- Star Pattern 3.py
+++ Star Pattern 3.py
@@ -9,6 +9,17 @@
 for i in range(4):
     for j in range(1,i+1):
         print(" ",end="")
-    for j in range(0,4):
+    for k in range(0,4):
         print("*",end="")
     print()        
+    
+    
+'''
+for i in range(4):
+    for j in range(i):
+        print(' ',end='')
+    for k in range(5):
+        print('*',end='')
+    print('')        
+    
+'''    
