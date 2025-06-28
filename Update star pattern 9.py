ec47ebe50cc814f40b8d6bb8fb7e8815e85c1cd0
Vijay Vaddi <141708089+vijayvaddi11@users.noreply.github.com>
2025-06-28 16:15:31 +0530

    Update and rename star pattern 9.py to Update star pattern 9.py

--- Update star pattern 9.py
+++ Update star pattern 9.py
@@ -13,8 +13,8 @@
 r=5
 n=r-1
 for i in range(r):
-    for j in range(n-i):
-        print(' ',end='')
+    for j in range(n-i): 
+        print(' ',end='') 
     temp = i*2+1
     for i in range(temp):
         print('*',end='')
