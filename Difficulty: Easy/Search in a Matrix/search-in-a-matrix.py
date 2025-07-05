a16e620ffc6e021711b9275e7c812fbbe6d1302f
Vijay Vaddi <141708089+vijayvaddi11@users.noreply.github.com>
2025-07-05 05:54:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Search in a Matrix/search-in-a-matrix.py
@@ -0,0 +1,13 @@
+#User function Template for python3
+
+class Solution:
+    
+    #Function to search a given integer in a matrix.
+    def searchMatrix(self,matrix, x): 
+    	# code here 
+    	for i in matrix:
+    	    if x in i:
+    	        return True
+    	        break
+            else:
+                continue
