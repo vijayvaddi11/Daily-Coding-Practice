0c4686e6197c58e62c8f4f529a6b73380868b21c
Vijay Vaddi <141708089+vijayvaddi11@users.noreply.github.com>
2025-10-08 11:35:00 +0530

    Time: 0 ms (100%), Space: 17.8 MB (61.15%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -1,15 +1,25 @@
 class Solution:
-    def lengthOfLastWord(self, s: str) -> int:
-        j=s.strip()
-        f=[]
-        u=""
-        for i in j:
-            if i==" ":
-                f.append(u)
-                u=""
-            else:
-                u+=i
-        if u:
-            f.append(u)
+    def stripRemover(self,s):
+        n=len(s)
+        start=0
+        end=n-1
 
-        return len(f[-1])  
+        while start<n and s[start]==" ":
+            start+=1
+
+        while end>0 and s[end]==" ":
+            end-=1
+
+        return s[start:end+1]    
+            
+          
+    def lengthOfLastWord(self, s: str) -> int:
+        remove_ex_spaces=self.stripRemover(s)
+        n=len(remove_ex_spaces)
+        count=0
+        for i in range(n-1,-1,-1):
+            if remove_ex_spaces[i]!=" ":
+                count+=1
+            else:
+                break
+        return count            
