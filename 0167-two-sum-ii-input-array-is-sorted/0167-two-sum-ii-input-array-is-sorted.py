accf2ea89165374646e12503cc29531a61769e2b
Vijay Vaddi <141708089+vijayvaddi11@users.noreply.github.com>
2025-10-09 11:27:06 +0530

    Time: 3 ms (80.99%), Space: 18.5 MB (59.44%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,26 +1,33 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        # n=len(numbers)
-        # start=0
-        # end=n-1
+        n=len(numbers)
+        start=0
+        end=n-1
 
-        # while start<end:
-        #     add_result = numbers[start]+numbers[end]
-        #     if add_result == target:
-        #         return [start+1, end+1]
-        #     elif add_result > target:
-        #         end-=1
-        #     elif add_result < target:
-        #         start+=1
+        while start<end:
+            add_result = numbers[start]+numbers[end]
+            if add_result == target:
+                return [start+1, end+1]
+            elif add_result > target:
+                end-=1
+            elif add_result < target:
+                start+=1
 
-        n = len(numbers)
-        dict1={}
-        for i in range(n):
-            res=target-numbers[i]
-            if res not in dict1:
-                dict1[numbers[i]]=i
-            else:
-                return [dict1[res]+1,i+1]    
+
+
+
+
+
+#best for unsorted list space comp = O(n)
+
+        # n = len(numbers)
+        # dict1={}
+        # for i in range(n):
+        #     res=target-numbers[i]
+        #     if res not in dict1:
+        #         dict1[numbers[i]]=i
+        #     else:
+        #         return [dict1[res]+1,i+1]    
 
 
 
