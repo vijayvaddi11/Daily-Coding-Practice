0734e3ebcb903b545cdde7143065d00a57314cfd
Vijay Vaddi <141708089+vijayvaddi11@users.noreply.github.com>
2025-11-04 08:34:41 +0530

    Time: 3 ms (65.17%), Space: 19.6 MB (76.85%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,25 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        result=0
+        for num in nums:
+            result^=num
+        return result
+
+
+
+
+        # tc-O(n), sc-O(n)
+        
+        # freq={}
+        # seen=set()
+        # for i in nums:
+        #     if i not in seen:
+        #         seen.add(i)
+        #         freq[i]=1
+        #     else:
+        #         freq[i]+=1
+        
+
+        # for key,value in freq.items():
+        #     if value==1:
+        #         return key
