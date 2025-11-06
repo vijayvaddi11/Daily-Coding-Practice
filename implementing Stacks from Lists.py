ecaecea0bb2575ceb0135110e31d93df761cb001
Vijay Vaddi <141708089+vijayvaddi11@users.noreply.github.com>
2025-11-06 11:30:35 +0530

    Update implementing Stacks from Lists.py
    Modified stack implementation to maintain a length attribute for size tracking.

--- implementing Stacks from Lists.py
+++ implementing Stacks from Lists.py
@@ -1,13 +1,15 @@
+#list implimentation of stack
 class Stack:
     def __init__(self):
         self.st=[]
-    
+        self.length=0
     def push(self,x):
+        self.length+=1
         self.st.append(x)
-    
     def pop(self):
         if len(self.st)==0:
             return -1
+        self.length-=1
         x=self.st[-1]
         self.st.pop()
         return x
@@ -17,12 +19,17 @@ class Stack:
         x=self.st[-1]
         return x
     def size(self):
-        return len(self.st)
-    
+        return self.length
+
 stack=Stack()
 stack.push(1)
 stack.push(2)
 stack.push(3)
+stack.push(4)
+stack.push(5)
 
+print(stack.size())
 print(stack.pop())
+
 print(stack.top())
+
