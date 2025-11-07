57fcddc882308a507e8ec36cb3675f09eef350c8
Vijay Vaddi <141708089+vijayvaddi11@users.noreply.github.com>
2025-11-07 09:48:39 +0530

    Time: 0 ms (100%), Space: 18.1 MB (21.05%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
@@ -1,33 +1,30 @@
 class MyQueue:
 
     def __init__(self):
-        self.stack1 = []
-        self.stack2 = []
+        self.stack1=[]
+        self.stack2=[]
+        
 
     def push(self, x: int) -> None:
-        # Always push to stack1
+        while len(self.stack1)>0:
+            self.stack2.append(self.stack1.pop())
         self.stack1.append(x)
+        while len(self.stack2)>0:
+            self.stack1.append(self.stack2.pop())
+        
 
     def pop(self) -> int:
-        # Move elements to stack2 if it's empty
-        if not self.stack2:
-            while self.stack1:
-                self.stack2.append(self.stack1.pop())
-        # Pop from stack2 (front of queue)
-        return self.stack2.pop()
+        x=self.stack1[-1]
+        self.stack1.pop()
+        return x
+        
 
     def peek(self) -> int:
-        # Move elements if needed
-        if not self.stack2:
-            while self.stack1:
-                self.stack2.append(self.stack1.pop())
-        # Return front element (top of stack2)
-        return self.stack2[-1]
+        return self.stack1[-1]
 
     def empty(self) -> bool:
-        # Queue is empty only if both stacks are empty
-        return not self.stack1 and not self.stack2
-
+        return len(self.stack1)==0
+        
 
 
 # Your MyQueue object will be instantiated and called as such:
