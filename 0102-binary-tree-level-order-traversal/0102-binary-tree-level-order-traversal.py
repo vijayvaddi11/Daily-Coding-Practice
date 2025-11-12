63deafd6b3c1f879f8d777c2b1783cf2debcd76d
Vijay Vaddi <141708089+vijayvaddi11@users.noreply.github.com>
2025-11-12 18:25:09 +0530

    Time: 7 ms (1.99%), Space: 18.5 MB (37.7%) - LeetHub

---
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -0,0 +1,61 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Queue:
+    def __init__(self):
+        self.q=[]
+        self.front=-1
+    
+    def push(self,x):
+        if self.front==-1:
+            self.front=0
+        self.q.append(x)
+    
+    def pop(self):
+        if self.front == -1:
+            return -1
+        x= self.q[self.front]
+        self.front+=1
+        if self.front == len(self.q):
+            self.front=-1
+            self.q=[]
+        return x
+    def getFront(self):
+        if self.front ==-1:
+            return -1
+        x=self.q[self.front]
+        return x
+    def size(self):
+        if self.front==-1:
+            return 0
+        return len(self.q)-self.front
+
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        ans=[]
+        if root is None:
+            return ans
+        queue=Queue()
+        queue.push(root)
+        ans.append([root.val])
+
+        while queue.size()>0:
+            l=queue.size()
+            level=[]
+            for i in range(l):
+                front=queue.pop()
+                if front.left!=None:
+                    queue.push(front.left)
+                    level.append(front.left.val)
+                if front.right!=None:
+                    queue.push(front.right)
+                    level.append(front.right.val)
+            if len(level)>0:
+                ans.append(level)
+        return ans
+
+        
