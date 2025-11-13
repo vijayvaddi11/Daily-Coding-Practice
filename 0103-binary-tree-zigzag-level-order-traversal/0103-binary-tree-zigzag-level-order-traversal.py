dcadb5008b0f939c33f6fc81e15caa2484f2d484
Vijay Vaddi <141708089+vijayvaddi11@users.noreply.github.com>
2025-11-13 08:19:22 +0530

    Time: 3 ms (6.44%), Space: 18.2 MB (7.88%) - LeetHub

---
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.py
@@ -0,0 +1,52 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Queue:
+    def __init__(self):
+        self.q=[]
+        self.front=-1
+    def push(self,x):
+        if self.front ==-1:
+            self.front=0
+        self.q.append(x)
+    def pop(self):
+        if self.front == -1:
+            return -1
+        x=self.q[self.front]
+        self.front+=1
+        if self.front==len(self.q):
+            self.q=[]
+            self.front=-1
+        return x
+    def size(self):
+        if self.front==-1:
+            return 0
+        return len(self.q)-self.front
+class Solution:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        ans=[]
+        if root == None:
+            return ans
+        q=Queue()
+        q.push(root)
+        count=0
+        while q.size()>0:
+            l=q.size()
+            level=[]
+            for i in range(l):
+                front=q.pop()
+                level.append(front.val)
+                if front.left:
+                        q.push(front.left)
+                if front.right:
+                    q.push(front.right)
+            if count%2==0:
+                ans.append(level)
+            elif count%2!=0:
+                ans.append(level[::-1])
+            count+=1
+        return ans
+        
