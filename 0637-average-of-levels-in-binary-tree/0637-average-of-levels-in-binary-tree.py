bd1c45c949d02a909e99eb26178aeeb4e0f3c46d
Vijay Vaddi <141708089+vijayvaddi11@users.noreply.github.com>
2025-11-13 16:49:50 +0530

    Time: 3 ms (46.19%), Space: 20 MB (13.03%) - LeetHub

---
+++ 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.py
@@ -0,0 +1,53 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Queue:
+    def __init__(self):
+        self.q=[]
+        self.front=-1
+    def push(self,x):
+        if self.front == -1:
+            self.front=0
+        self.q.append(x)
+    def pop(self):
+        if self.front==-1:
+            return -1
+        x = self.q[self.front]
+        self.front+=1
+        if self.front==len(self.q):
+            self.front=-1
+            self.q=[]
+        return x
+    def size(self):
+        if self.front==-1:
+            return 0
+        return len(self.q)-self.front
+    
+        
+
+class Solution:
+    def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
+        ans=[]
+        if root is None:
+            return ans
+        q=Queue()
+        q.push(root)
+        while q.size()>0:
+            l=q.size()
+            level=[]
+            for i in range(l):
+                front=q.pop()
+                level.append(front.val)
+                if front.left:
+                    q.push(front.left)
+                if front.right:
+                    q.push(front.right)
+            avg= sum(level)/len(level)
+            ans.append(avg)
+            
+        return ans
+
+        
