1feb86478ff841d2aded3eb1d99e34705ba7df1e
Vijay Vaddi <141708089+vijayvaddi11@users.noreply.github.com>
2025-11-18 14:24:45 +0530

    Update MinHeap(manually).py

--- MinHeap(manually).py
+++ MinHeap(manually).py
@@ -15,7 +15,7 @@ class MinHeap:
             self.h(parent),self.h[i]=self.h[i],self.h[parent]
             i=parent
     def pop(self):
-        if len(self.h) is None:
+        if len(self.h) ==0:
             return None
         self.h[0],self.h[-1]=self.h[-1],self.h[0]
         val=self.h.pop()
