031f2e7312e3cfc7e6531df1ef290c05a124364b
Vijay Vaddi <141708089+vijayvaddi11@users.noreply.github.com>
2025-12-01 11:15:01 +0530

    Time: 11 ms (72.52%), Space: 17.7 MB (97.43%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,26 +1,59 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        # if len(s)!= len(t):
+        #     return False 
+        # freq1={}
+        # for i in s:
+        #     if i not in freq1:
+        #         freq1[i]=1
+        #     else:
+        #         freq1[i]+=1
+        
+        # freq2={}
+        # for j in t:
+        #     if j not in freq2:
+        #         freq2[j]=1
+        #     else:
+        #         freq2[j]+=1
+        
+        # for key in freq1.keys():
+        #     if key not in freq2 or freq1[key]!=freq2[key]:
+        #         return False
+        # return True
+
+
         if len(s)!= len(t):
             return False 
-        freq1={}
+
+        freq={}
+
         for i in s:
-            if i not in freq1:
-                freq1[i]=1
+            if i not in freq:
+                freq[i]=1
             else:
-                freq1[i]+=1
+                freq[i]+=1
         
-        freq2={}
         for j in t:
-            if j not in freq2:
-                freq2[j]=1
+            if j in freq:
+                freq[j]-=1
             else:
-                freq2[j]+=1
-        
-        for key in freq1.keys():
-            if key not in freq2 or freq1[key]!=freq2[key]:
                 return False
-        return True
+
+        ans=0
+        for val in freq.values():
+            if val<0:
+                return False
+            else:
+                ans+=val
         
+        if ans==0:
+            return True
+        else: 
+            return False
+        
+        #using single hashmap more optimal than double 
+        #space comp-O(n)
+        #time Comp-O(n)
         
 
         
