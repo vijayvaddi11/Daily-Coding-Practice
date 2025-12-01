833a3aed3f6ac5978f424573cd7ea35bc18d7cfb
Vijay Vaddi <141708089+vijayvaddi11@users.noreply.github.com>
2025-12-01 07:11:55 +0530

    Time: 13 ms (62.3%), Space: 31.6 MB (29.5%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,18 +1,26 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         #using dict
-        freq={}
+        # freq={}
+        # seen=set()
+        # for i in nums:
+        #     if i not in seen:
+        #         seen.add(i)
+        #         freq[i]=1
+        #     else:
+        #         freq[i]+=1
+        
+        # for i in freq.values():
+        #     if i>1:
+        #         return True
+        # return False
+
         seen=set()
         for i in nums:
-            if i not in seen:
-                seen.add(i)
-                freq[i]=1
-            else:
-                freq[i]+=1
-        
-        for i in freq.values():
-            if i>1:
+            if i in seen:
                 return True
+            seen.add(i)
         return False
+        
 
 
