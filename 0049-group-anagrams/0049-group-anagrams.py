8a23a432bdc6017989450a50044344bef823066d
Vijay Vaddi <141708089+vijayvaddi11@users.noreply.github.com>
2025-12-03 05:59:20 +0530

    Time: 19 ms (37.06%), Space: 22.5 MB (34.68%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,9 +1,21 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         #using sorting
-        res = defaultdict(list)
+        # res = defaultdict(list)
+
+        # for i in strs:
+        #     sortedI = "".join(sorted(i))
+        #     res[sortedI].append(i)
+        # return list(res.values())
+
+
+        #hashmap
+        res=defaultdict(list)
 
         for i in strs:
-            sortedI = "".join(sorted(i))
-            res[sortedI].append(i)
+            count = [0]*26
+            for s in i:
+                count[ord(s)-ord('a')]+=1
+            res[tuple(count)].append(i)
         return list(res.values())
+
