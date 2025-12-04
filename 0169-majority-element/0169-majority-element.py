325e678fcda21f8aae2a7ba557de2f25513c3681
Vijay Vaddi <141708089+vijayvaddi11@users.noreply.github.com>
2025-12-04 16:54:25 +0530

    Time: 1 ms (87.1%), Space: 19.4 MB (19.14%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,14 +1,29 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        freq = defaultdict(int)
+        # freq = defaultdict(int)
+        # for i in nums:
+        #     freq[i]+=1
+        # max_key=0
+        # max_val=0
+        # for key,val in freq.items():
+        #     if val>max_val:
+        #         max_val=val
+        #         max_key=key
+        # return max_key
+
+
+        #Boyer-Moore Voting Algorithm
+        #tc- O(n) and sc- O(1)
+        count=0
+        res=0
         for i in nums:
-            freq[i]+=1
-        max_key=0
-        max_val=0
-        for key,val in freq.items():
-            if val>max_val:
-                max_val=val
-                max_key=key
-        return max_key
+            if count==0:
+                res=i
+                count=1
+            elif res == i:
+                count+=1
+            else:
+                count-=1
+        return res
     
              
