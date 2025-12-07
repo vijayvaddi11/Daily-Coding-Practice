ac233c6134cef95e24cdeb443e4b8af52d5d38be
Vijay Vaddi <141708089+vijayvaddi11@users.noreply.github.com>
2025-12-07 07:50:44 +0530

    Time: 0 ms (100%), Space: 17.6 MB (97.24%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,22 +1,18 @@
 class Solution:
-    def sortColors(self, nums: List[int]) -> None:
+    def sortColors(self, n: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        n=len(nums)
+        l,r = 0,len(n)-1
         i=0
-        left=0
-        right=n-1
-
-        while i<=right:
-            if nums[i]==1:
-                i+=1
-            elif nums[i]==0:
-                nums[left],nums[i]=nums[i],nums[left]
-                i+=1
-                left+=1
-            else:
-                nums[i],nums[right]=nums[right],nums[i]
-                right-=1
-
-        return nums        
+        while i <= r:
+            if n[i]==2:
+                n[r],n[i]=n[i],n[r]
+                i-=1
+                r-=1
+            elif n[i]==0:
+                n[l],n[i]=n[i],n[l]
+                l+=1
+            
+            i+=1
+                
