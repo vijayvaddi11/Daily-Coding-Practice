a5c86860bd4ad4e7e999e6465b5907701ca413f7
Vijay Vaddi <141708089+vijayvaddi11@users.noreply.github.com>
2025-12-11 06:25:39 +0530

    Time: 6 ms (41.41%), Space: 18 MB (24.54%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -0,0 +1,31 @@
+class Solution:
+    def isValidSudoku(self, n: List[List[str]]) -> bool:
+        for i in range(0,9):
+            seen1=set()
+            seen2=set()
+            for j in range(0,9):
+                if n[i][j] in seen1:
+                    return False
+                    break
+                else:
+                    if n[i][j]!='.':
+                        seen1.add(n[i][j])
+                if n[j][i] in seen2:
+                    return False
+                    break
+                else:
+                    if n[j][i]!='.':
+                        seen2.add(n[j][i])
+        for square in range(9):
+            seen = set()
+            for i in range(3):
+                for j in range(3):
+                    row = (square//3) * 3 + i
+                    col = (square % 3) * 3 + j
+                    if n[row][col] == ".":
+                        continue
+                    if n[row][col] in seen:
+                        return False
+                    seen.add(n[row][col])
+        return True
+        
