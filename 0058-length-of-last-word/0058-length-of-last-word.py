cdb434036ceed8355c474794a5ca89e5052319bf
Vijay Vaddi <141708089+vijayvaddi11@users.noreply.github.com>
2026-03-21 22:07:53 +0530

    Time: 0 ms (100%), Space: 19.3 MB (78%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -1,26 +1,9 @@
 class Solution:
-    #manually without using inbuild fn
-    def stripRemover(self,s):
-        n=len(s)
-        start=0
-        end=n-1
-
-        while start<n and s[start]==" ":
-            start+=1
-
-        while end>0 and s[end]==" ":
-            end-=1
-
-        return s[start:end+1]    
-            
-          
     def lengthOfLastWord(self, s: str) -> int:
-        remove_ex_spaces=self.stripRemover(s)
-        n=len(remove_ex_spaces)
+        n = s.strip()
         count=0
-        for i in range(n-1,-1,-1):
-            if remove_ex_spaces[i]!=" ":
-                count+=1
-            else:
+        for i in n[::-1]:
+            if i == " ":
                 break
-        return count            
+            count+=1
+        return count
